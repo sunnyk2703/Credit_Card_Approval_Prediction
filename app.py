78aeff3d6bf6d004f75a35616b561c14d7490f39
sunnycodes <sunnykannadi327@gmail.com>
2026-07-12 21:52:34 +0530

    Fix Render deployment

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def predict():
     except Exception as e:
         print("ERROR:", e)
         return jsonify({"prediction": str(e)}), 500
-
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
